e14cdfabe1b78f0a3963c7b0bda27c17532212b7
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-10-24 16:58:45 -0300

    guardando

---
+++ 2014-c2_Parcial-2-Com3-T2/detalle_longitudes.py
@@ -0,0 +1,19 @@
+""" Definir  una  función  “detalle_longitudes”,  que  reciba  como  parámetro  una  tupla  compuesta
+por cadenas de caracteres que representan palabras, y devuelva como resultado un diccionario
+en donde las claves sean las longitudes de esas palabras, y los valores, la lista respectiva con
+las palabras que tienen esa longitud. [No es necesario validar los datos.]
+Ejemplo:   Dada  la  tupla = (' no', 'se', 'lamente', 'la', 'mente', 'inteligente ')
+la función debería devolver (en cualquier orden)
+{ 2 : ['no', 'se', 'la'] , 11: ['inteligente'] , 5 : ['mente'] , 7 : ['lamente'] } """
+
+def detalle_longitudes(tupla):
+    diccionarioLongitudes = {}
+    for i in tupla:
+        # print(i)
+        longitud = len(i)
+        if (longitud not in diccionarioLongitudes):
+            diccionarioLongitudes[longitud] = []
+            print(f"{i} {diccionarioLongitudes}")
+
+tupla = ('no', 'se', 'lamente', 'la', 'mente', 'inteligente')
+detalle_longitudes(tupla)
