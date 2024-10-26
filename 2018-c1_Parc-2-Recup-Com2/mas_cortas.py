1da1f94d0e1c32652c2c1e06caa9edecc036e13c
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-10-25 22:48:18 -0300

    guardando

--- 2018-c1_Parc-2-Recup-Com2/mas_cortas.py
+++ 2018-c1_Parc-2-Recup-Com2/mas_cortas.py
@@ -1,11 +1,10 @@
-""" a. 1) Definir una función, denominada “mas_cortas”, que reciba como parámetros una cadena
+"""Definir una función, denominada “mas_cortas”, que reciba como parámetros una cadena
 (cad)  de  caracteres  y  una  tupla  (tup)  integrada  por  cadenas  de  caracteres,  y  devuelva  como
 resultado  una  cadena  compuesta  con  la  concatenación  de  las  cadenas  de  la  tupla  tup  cuya
 longitud sea inferior a la de la cadena cad. [No es necesario validar los datos]
 Ejemplo: Dados la cadena "empleo" y  la tupla
 ('ver','piloto','de','drones','tambien','cerveza','artesanal')
-la función debería devolver la cadena "verde"
-2) Dar un ejemplo de invocación y utilización de “mas_cortas”, desde una función “main". """
+la función debería devolver la cadena "verde". """
 
 def mas_cortas(cad, tup):
     cadena = ""
